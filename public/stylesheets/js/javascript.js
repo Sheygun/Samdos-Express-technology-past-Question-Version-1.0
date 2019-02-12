7957aa9ec4721d02bb5f32e0c885e3740f7fd9d7
Olabayo Segun Emmanuel <hollasheg@gmail.com>
2019-02-12 17:23:16 -0500

    70% of version 1.0 project

---
+++ public/stylesheets/js/javascript.js
@@ -0,0 +1,70 @@
+// -------------------------------------------------CSC330 start here ----------------------------------------------------------------
+
+function getPinCss320161734(){
+    let pass1 = 1234;
+    let userPin = document.getElementById('userPin').value;
+    if (userPin == ""){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You didn't enter any Value, Please Input Pin!!!";
+    }else if (userPin != pass1){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You've entered an Incorrect pin, kindly try again!!!";
+    }
+    else{
+        window.location.replace('/csc32020180101seo1');
+    }
+}
+
+function getPinCss320161756(){
+    let pass1 = 1234;
+    let userPin = document.getElementById('userPin').value;
+    if (userPin == ""){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You didn't enter any Value, Please Input Pin!!!";
+    }else if (userPin != pass1){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You've entered an Incorrect pin, kindly try again!!!";
+    }
+    else{
+        window.location.replace('/csc32020180101seo2');
+    }
+}
+
+// ---------------------------------------------------------------------end here --------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------CSC320 start here ----------------------------------------------------------------
+
+
+
+function getPinCss332161723(){
+    let pass1 = 5678;
+    let userPin = document.getElementById('userPin').value;
+    if (userPin == ""){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You didn't enter any Value, Please Input Pin!!!";
+    }else if (userPin != pass1){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You've entered an Incorrect pin, kindly try again!!!";
+    }
+    else{
+        window.location.replace('/csc3322018010102seo1');
+    }
+}
+
+function getPinCss3321617456(){
+    let pass1 = 5678;
+    let userPin = document.getElementById('userPin').value;
+    if (userPin == ""){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You didn't enter any Value, Please Input Pin!!!";
+    }else if (userPin != pass1){
+        document.getElementById('danger').style.visibility = "visible";
+        document.getElementById('print-error').innerHTML = "You've entered an Incorrect pin, kindly try again!!!";
+    }
+    else{
+        window.location.replace('/csc3322018010103seo1');
+    }
+}
+
+
+// --------------------------------------------------------------------CSC320 end here ----------------------------------------------------------------
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+// --------------------------csc320 ------------------------------------
+router.get('/csc32020180101seo', function(req, res) {
+  res.render('questions/csc320/csc320', { title: 'Express' });
+});
+
+router.get('/csc32020180101seo1', function(req, res) {
+  res.render('questions/csc320/csc320two', { title: 'Express' });
+});
+
+router.get('/csc32020180101seo2', function(req, res) {
+  res.render('questions/csc320/csc320three', { title: 'Express' });
+});
+
+// --------------------------csc332 ------------------------------------
+router.get('/error', function(req, res) {
+  res.render('questions/unprocessed', { title: 'Express' });
+});
+
+router.get('/csc3322018010101seo1', function(req, res) {
+  res.render('questions/csc322/csc332', { title: 'Express' });
+});
+
+router.get('/csc3322018010102seo1', function(req, res) {
+  res.render('questions/csc322/csc332two', { title: 'Express' });
+});
+
+router.get('/csc3322018010103seo1', function(req, res) {
+  res.render('questions/csc322/csc332three', { title: 'Express' });
+});
 module.exports = router;
